c88d0a9be13d037df66f01faaa9381d252db519d
rowshannahar3 <samirowshan@gmail.com>
2026-09-13 21:22:54 +0600

    added  base name in vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/Navbar";
 export default function App() {
   return <div className="min-h-screen bg-white text-slate-900"><Navbar /></div>;
